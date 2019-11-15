7139e3da46936e31fe7252dedd6a1ec953204341
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2019-11-15 15:58:21 +0100

    Practice 2 exercise 4 added

---
+++ Practices/Practice 2/Exercise4.hs
@@ -0,0 +1,27 @@
+-- Copyright © 2019
+--
+-- Francisco Javier Blázquez Martínez ~ frblazqu@ucm.es
+--
+-- Double degree in Mathematics-Computer engineering.
+-- Complutense university, Madrid.
+--
+-- Description:
+-- Implement the following operators over nonempy lists:
+
+
+-- a) foldr1 f [x1,...,xn] = f(x1 f (x2 f( ... f( xn )...)))
+
+
+
+
+
+
+
+
+-- b) foldr1 f [x1,...,xn] = f(( ... f(f(x1) x2)...) xn)
+
+
+
+
+
+
